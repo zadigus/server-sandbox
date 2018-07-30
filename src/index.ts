90125b943828f6ca72b601ab168ecdba27aea469
Laurent Michel <laurent1984@gmail.com>
2018-07-30 22:26:06 +0200

    * clean up index file

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-// import container from "./config/TestConfig";
-// import Requester, { T_ResponseContent } from '../spec/steps/Requester';
-
-// const requester: Requester = container.get<Requester>(Requester);
-// requester.get("/user");
-// requester.lastResponse.then((res: T_ResponseContent) => console.log("response = " + JSON.stringify(res)));
