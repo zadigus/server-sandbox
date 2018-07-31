a635062c66d23008737efda19e9405b9b56786cb
Laurent Michel <laurent1984@gmail.com>
2018-07-31 14:30:15 +0200

    test using external repo

--- spec/steps/ServerRequest/IServer.ts
+++
@@ -1,5 +0,0 @@
-import { T_Url, T_Response } from "./Requester/ResponseData";
-
-export default interface IServer {
-  get(url: T_Url): T_Response;
-}
--- spec/steps/ServerRequest/constants/Identifiers.ts
+++
@@ -1,6 +0,0 @@
-const SERVICE_IDENTIFIER: any = {
-  SERVER_APP: Symbol.for("ServerApp"),
-  SERVER: Symbol.for("Server")
-};
-
-export default SERVICE_IDENTIFIER;
