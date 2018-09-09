04a625c15652453a5b7500e5026ff2cbca34058b
Laurent Michel <laurent1984@gmail.com>
2018-09-09 09:55:35 +0200

    define minimal server for testing

--- src/App/App.ts
+++ src/App/App.ts
@@ -5,6 +5,10 @@ import Test from "../Test";
 const app: Express = express();
 const myTest: Test = new Test();
 
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).send("OK");
+});
+
 app.get("/user", (req: Request, res: Response) => {
   myTest.test();
   res.status(200).json({ name: "john" });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 import app from "App/App";
 
-app.listen(3000);
+const port = process.env.PORT || 3000;
+app
+  .listen(3000)
+  .on("listening", () => console.log(`Server started at port ${port}`));
